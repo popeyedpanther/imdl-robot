1896381246efabd6605d333cdae9f1a1482157b6
Patrick N <patrick.neal.j@gmail.com>
2015-11-22 23:03:59 -0500

    Added Sketch for arduino on top level

---
+++ Arduino_Node/Top_Level_Arduino/Top_Level_Arduino.ino
@@ -0,0 +1,15 @@
+
+// Tilt servo should cyle through 60-140 with 140 facing down and 60 facing up
+// Pan servo should be cycled through 0-180 with 0 facing right
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
