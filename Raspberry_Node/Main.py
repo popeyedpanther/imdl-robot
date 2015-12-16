cbfe3a9524a33ad194418285353e2071abd0ebbc
Patrick N <patrick.neal.j@gmail.com>
2015-12-16 00:54:46 -0500

    Updated align behavior

--- Raspberry_Node/Main.py
+++ Raspberry_Node/Main.py
@@ -74,7 +74,7 @@ while continuousRun:
         previousClock = currentClock
         Bob.requestMega()
         Bob.requestUno()
-        print Bob.foundObject
+        print Bob.OAOverride
 
     # Behavior changes
     if Bob.foundObject != 1:      # Searches for the block
@@ -87,11 +87,11 @@ while continuousRun:
         if Bob.behavior != 2:
             Bob.updateBehavior(2)
             if not Bob.OAOverride:
-                Bob.move('S',0)
+                Bob.move('S', 0)
             print "I found an object"
 
     elif pickedupObject and not droppedBlock:     # Should localize and drop the block
-        ## Should cross of that block from search list
+        # Should cross of that block from search list
         # Bob.localize()
 
         if Bob.behavior != 3:
@@ -113,27 +113,40 @@ while continuousRun:
         # Do what is necessary to find a block
         # Issue movement commands to the Arduino Mega
         # Perform  some random movements here/drive around
-        if not Bob.OAOverride and Bob.motionComplete:
+        if not Bob.OAOverride and Bob.motionComplete == 1:
             randDirection = random.randint(1, 9)
             if 1 <= randDirection <= 3:
                 # Go forward some amount
-                Bob.move('F', 20)
+                Bob.move('F', random.randint(10, 20))
 
             elif 4 <= randDirection <= 5:
                 # Reverse some amount
-                Bob.move('R', 20)
+                Bob.move('R', random.randint(10, 20))
 
             elif 5 <= randDirection <= 7:
                 # Turn left some amount
-                Bob.move('L', 45)
+                Bob.move('L', random.randint(20, 45))
 
             else:
                 # Turn right some amount
-                Bob.move('R', 45)
+                Bob.move('R', random.randint(20, 45))
 
     elif Bob.behavior == 2:
         # Do what is necessary to pick up the block
         # Align with the block in x direction and then the y direction
+        xError = (160 - Bob.objectX)
+
+        if(abs(xError)) > 15:
+            xError = copysign(1, xError)*15
+
+        if copysign(1, xError) > 0 and Bob.motionComplete == 1:
+            Bob.move('R', abs(xError))
+        elif copysign(1, xError) < 0 and Bob.motionComplete == 1:
+            Bob.move('L', abs(xError))
+
+        if 145 < Bob.objectX < 180:
+            # Robot is aligned
+            robotAligned = True
 
         # Then go to pick it up
         if robotAligned:
@@ -150,6 +163,7 @@ while continuousRun:
             sleep(0.2)
             Bob.move('W', 160)
             sleep(0.2)
+            pickedupObject = True
 
     elif Bob.behavior == 3:
         droppedBlock = True
